5ab2b00025006a9beddd9717458b05b2d5ce204b
Adrián López Pisá <adrianlopezpisa@gmail.com>
2021-09-19 12:41:32 +0200

    Create vue project

---
+++ reuniver-webpage/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>
+      <p>
+        Aquí irá el menú superior
+      </p>
+    </div>
+    <SectionTemplate sectionType=""/>
+  </div>
+</template>
+
+<script>
+import SectionTemplate from './components/SectionTemplate.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SectionTemplate
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ reuniver-webpage/src/components/SectionTemplate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-section-div">
+    <h2> Título de la sección </h2>
+    <div>Lorem ipsum dolor</div>
+    <hr style="width:80%">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionTemplate',
+  props: {
+    sectionType: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
